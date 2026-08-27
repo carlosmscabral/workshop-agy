d9f8f2b508f73cb89c2f6276f3061a1c986e0618
Carlos Cabral <carloscabral@google.com>
2026-08-27 20:25:06 +0000

    Fix Terraform invalid count error by removing module depends_on

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,18 @@
-resource "google_storage_bucket" "geap_bucket" {
-  name                        = "${var.gcp_project_id}-geap-artifacts"
-  location                    = var.gcp_region
-  uniform_bucket_level_access = true
-  force_destroy               = true
-}
-
 module "cli" {
   source  = "terraform-google-modules/gcloud/google"
   version = "~> 3.0.1"
 
   platform = "linux"
 
+  additional_components = ["beta"]
+
   create_cmd_entrypoint = "chmod +x ${path.module}/scripts/script.sh; ${path.module}/scripts/script.sh"
-  create_cmd_body       = "${var.gcp_project_id} ${var.gcp_region} ${var.gcp_zone} ${var.service_account_key_file}"
+  create_cmd_body       = "${var.gcp_project_id} ${var.gcp_region} ${var.gcp_zone}"
 
   skip_download = false
   upgrade       = false
 
-  service_account_key_file = var.service_account_key_file
+  gcloud_sdk_version = "420.0.0"
 
-  depends_on = [google_storage_bucket.geap_bucket]
+  service_account_key_file = var.service_account_key_file
 }
